999f2531bc53db00c403e72d968f7de443f8bf1f
David Wisecarver <dwisecar@gmail.com>
2020-10-19 02:26:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,11 +10,15 @@ def using_pop(array)
   array.pop
 end
 
+def pop_with_args(array)
+  array.pop(2)
+end
+
 def using_shift(array)
   array.shift
 end
 
-def pop_with_args(array)
+def shift_with_args(array)
   array.shift(2)
 end
 
